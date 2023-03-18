b6e908e2696f6863498c66fcb3a8f45cf4fa5fee
Granj93 <www.raven.nick@yandex.ru>
2023-03-18 12:24:36 +0300

    Выделен вспомогательный класс для управления сессиями SessionHelper

--- tests/tst_add_group.py
+++ tests/tst_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
 from models.group import Group
-from Fixtures.application_group import Application
+from fixtures.application_group import Application
 
 
 @pytest.fixture
@@ -12,6 +12,6 @@ def app(request):
 
 
 def test_tst_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.fill_group_form(Group(name="tst_group_name", header="tst_logo", footer="tst_comment"))
-    app.logout()
+    app.session.logout()
